c073aef5cc2ecb5951ef4db4ad49d73e8229b568
emi.dev <vmarcosvini33@gmail.com>
2026-06-08 12:03:31 -0300

    exec2

---
+++ testeJson.js
@@ -0,0 +1,20 @@
+let notaFiscal = {
+    nomeComprador: "Marcos Campos",
+    cpf: "123.456.789-00",
+    dataCompra: "2024-06-01",
+    itens: [
+        {
+            nome: "Macbook Neo 15",
+            quantidade: 1,
+            precoUnitario: 4400.00,
+        },
+        {
+            nome: "Teclado 60% RGB Wireless HyperX",
+            quantidade: 1,
+            precoUnitario: 250.00,
+        }
+    ],
+    precoTotal: 4650.00
+}
+
+console.log(notaFiscal);
